b052053446934d54caffd7cf85f153b6fd2a6371
rnik23 <147942574+rnik23@users.noreply.github.com>
2025-06-19 11:58:47 -0400

    Refactor node initialization to reduce API calls

--- data_streaming_prj.py
+++ data_streaming_prj.py
@@ -41,12 +41,34 @@ def get_current_weather(latitude, longitude):
         return None, None
 
 
-try:
-    from kafka import KafkaProducer
-except Exception:  # pragma: no cover - kafka is optional
-    KafkaProducer = None
+    def __init__(
+        self,
+        node_id: str,
+        latitude: float,
+        longitude: float,
+        temperature: float,
+        humidity: float,
+        wind_direction: str = "N",
+    ):
+        self.temperature = temperature
+        self.humidity = humidity
 
-# ---------------------------------------------------------------------------
+    temp, hum = get_current_weather(center_lat, center_long)
+    if temp is None:
+        temp = random.uniform(15, 25)
+    if hum is None:
+        hum = random.uniform(30, 50)
+
+            nodes.append(
+                IoTNode(
+                    node_id,
+                    latitude,
+                    longitude,
+                    temperature=temp,
+                    humidity=hum,
+                    wind_direction=wind_direction,
+                )
+            )
 # Weather helpers
 # ---------------------------------------------------------------------------
 
